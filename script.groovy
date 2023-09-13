dbce3c184ed65be67474ed4afe2b4101b6e5b6e0
flaviassantos <ssantosflavia@hotmail.com>
2023-09-13 11:13:42 +0200

    fix: fix readVersion function - fetch tags

--- script.groovy
+++ script.groovy
@@ -1,5 +1,6 @@
 def readVersion() {
     echo 'reading the application version...'
+    sh "git fetch --tags"
     def latestVersion = sh(returnStdout: true, script: 'git describe --tags --abbrev=0 --match *.*.* 2> /dev/null || echo 0.0.0').trim()
     env.TAG = latestVersion
     echo "version: ${TAG}"
